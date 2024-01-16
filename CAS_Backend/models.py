f314d57498e5e88dad13abbf4af2dc35960cbfce
Starlight5234 <starlight5234@protonmail.ch>
2024-01-17 00:10:06 +0530

    Add Models
    Change-Id: I96a8e9e799cfc590696bd79515ba5da675d456c3
    Signed-off-by: Starlight5234 <starlight5234@protonmail.ch>

--- CAS_Backend/models.py
+++ CAS_Backend/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+from decimal import Decimal, ROUND_HALF_EVEN
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 # Create your models here.
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.PositiveIntegerField(null=True)
+    phone_number = models.CharField(max_length=10)
+    monthly_salary = models.DecimalField(max_digits=10, decimal_places=2)
+    approved_limit = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    current_debt = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+class Loan(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    loan_id = models.AutoField(primary_key=True)
+    loan_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    tenure = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(180)]) # Set to 180 for now acc to data given
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    monthly_installment = models.DecimalField(max_digits=10, decimal_places=2)
+    emis_paid_on_time = models.PositiveIntegerField(default=0)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    repayments_left = models.PositiveIntegerField(default=0)
