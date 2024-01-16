e83a37bf00b89066c43b0d8b5748fd8ebb07ab32
Starlight5234 <starlight5234@protonmail.ch>
2024-01-17 01:02:07 +0530

    Skeleton for API Endpoints using REST
    Change-Id: Ia8c462ac439e5267fec3a039cea6cfa4618c4a87
    Signed-off-by: Starlight5234 <starlight5234@protonmail.ch>

--- CAS_Backend/views.py
+++ CAS_Backend/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.core.serializers import serialize
+from django.http import JsonResponse
+
 # Create your views here.
+def api_home(request):
+    return JsonResponse({'message': 'Welcome to the API!'})
